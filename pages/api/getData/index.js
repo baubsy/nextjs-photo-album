371b5a5311b1e7e97702d082deb95238e75ae72c
Baubs <pathogenic@gmail.com>
2023-02-21 23:31:20 -0600

    fauna testing

---
+++ pages/api/getData/index.js
@@ -0,0 +1,30 @@
+const {Ref} = require('faunadb');
+const faunadb = require("faunadb");
+
+import faunaKey from '../../../secrets.js'
+
+const q = faunadb.query;
+const secret = faunaKey;
+
+const client = new faunadb.Client({
+    secret,
+    domain: "db.fauna.com"
+});
+
+module.exports = async(req, res) => {
+    try {
+        const dbs = await client.query(
+            q.Map(
+                q.Paginate(q.Documents(q.Collection("photos"))),
+                q.Lambda("doc", q.Get(q.Var("doc")))
+            )
+        );
+        res.status(200).json(dbs.data);
+    } catch(error) {
+        res.status(500).json({error: error.message})
+    }
+
+    
+
+
+}
---
+++ secrets.js
@@ -0,0 +1,3 @@
+const faunaKey = "fnAE9YPYoGAAQ4cPKfF7mK-vxlfAbi15oC29JMHe";
+
+export default faunaKey;
