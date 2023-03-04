1bc347a066e68aae588bd2311a051ee0175ac488
Baubs <pathogenic@gmail.com>
2023-03-03 23:31:25 -0600

    hooking up upload form to imgur upload

---
+++ components/UploadPhotoForm.js
@@ -0,0 +1,40 @@
+import { FormControl, TextField, Button } from "@mui/material";
+import {useState, useEffect} from 'react';
+import imgurUpload from "../lib/Imgur";
+
+
+const UploadPhotoForm = () => {
+  const [photoName, setPhotoName] = useState("");
+  const [photo, setPhoto] = useState();
+
+
+  const onClick = () => {
+    //console.log(photoName);
+    //console.log(photo);
+    imgurUpload(photo);
+  }
+
+  return (
+    <FormControl>
+      <TextField 
+        id="photoName" 
+        variant="standard"
+        value={photoName}
+        onChange={(event) => {        
+          setPhotoName(event.target.value);
+        }}
+        />
+      <input
+        accept="image/*"
+        type="file"
+        onChange={(event)=>{
+          //console.log(event);
+          setPhoto(event.target.files[0]);
+        }}
+      />
+      <Button variant="contained" onClick={onClick}>Upload</Button>
+    </FormControl>
+  )
+};
+
+export default UploadPhotoForm;
